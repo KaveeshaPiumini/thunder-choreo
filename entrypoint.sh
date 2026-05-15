eea5e9925b180ed7dcb0ed3217c37fb2a5aecead
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2026-05-15 11:33:18 +0530

    fix: Use native env var resolution for DB password to fix read-only crash

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,6 @@ export BASE_URL="https://b029b391-50fb-49de-9264-d8924e1b1c39.e1-us-east-azure.c
 export PUBLIC_URL="${BASE_URL}"
 export SERVER_PUBLIC_URL="${BASE_URL}"
 
-# Inject database password from environment variable
-if [ -n "$AIVEN_DB_PASSWORD" ]; then
-    sed -i "s|DATABASE_PASSWORD_PLACEHOLDER|$AIVEN_DB_PASSWORD|g" "$THUNDER_HOME/repository/conf/deployment.yaml"
-fi
 
 echo ">>> Starting Thunder (without consent server)..."
 exec "$THUNDER_HOME/start.sh" --without-consent
